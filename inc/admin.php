0ab65f649f8e6cb257e7102baf0ff276420919a1
peterbooker <peter@kebopowered.com>
2013-12-04 18:59:05 +0000

    removed title placeholder rewrite, not needed

--- inc/admin.php
+++ inc/admin.php
@@ -8,27 +8,6 @@ if ( ! defined( 'KBTE_VERSION' ) ) {
     die;
 }
 
-/*
- * Customise the Title Placeholder for Testimonials.
- */
-function kbte_testimonials_title_placeholder( $title ) {
-
-    // Gets current post type
-    $screen = get_current_screen();
-    $post_type = $screen->post_type;
-    
-    // Only change for the right post type.
-    if ( 'kbte_testimonials' == $post_type ) {
-        
-        $title = apply_filters( 'kbte_testimonials_title_placeholder', 'Enter client name(s) here', $post_type );
-        
-    }
-
-    return $title;
-    
-}
-add_filter( 'enter_title_here', 'kbte_testimonials_title_placeholder' );
-
 /**
  * Edit the Admin List Titles for Testimonials.
  */
