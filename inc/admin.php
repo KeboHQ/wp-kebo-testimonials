c87cbf1573cb8a64c7965fd74bb1e42065687fba
peterbooker <peter@kebopowered.com>
2013-12-04 17:25:06 +0000

    template admin query function

--- inc/admin.php
+++ inc/admin.php
@@ -60,3 +60,28 @@ function kbte_testimonials_sortable_admin_columns( $columns ) {
     
 }	
 add_filter( 'manage_edit-kbte_testimonials_sortable_columns', 'kbte_testimonials_sortable_admin_columns' );
+
+/**
+ * Custom Post Type Archive Pagination Limits.
+ */
+function kbte_testimonials_admin_query( $query ) {
+
+    // Testimonials Admin Query
+    if ( is_admin() && $query->is_main_query() ) {
+
+        // Set Testimonials per Page as per user option
+        if ( isset( $query->query_vars['post_type'] ) && ( 'kbte_testimonials' == $query->query_vars['post_type'] ) ) {
+
+            // Orders by the Menu Order attribute
+            //$query->set('orderby', 'menu_order');
+            // Ascending order (1 first, etc).
+            //$query->set('order', 'ASC');
+
+            return;
+            
+        }
+
+    }
+
+}
+add_filter( 'pre_get_posts', 'kbte_testimonials_admin_query', 1 );
