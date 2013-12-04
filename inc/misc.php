4f969904ca7ad3bad1f4f8234d9deef077e9f492
peterbooker <peter@kebopowered.com>
2013-12-04 17:50:26 +0000

    added flush rewrite rules function

--- inc/misc.php
+++ inc/misc.php
@@ -2,3 +2,25 @@
 /* 
  * Kebo Testimonials - Misc/Helper Functions
  */
+
+/**
+ * Flush Rewrite Rules.
+ * Use if slug changes or plugin is installed/uninstalled.
+ */
+function kbte_flush_rewrite_rules() {
+    
+    global $pagenow, $wp_rewrite;
+
+    if ( 'admin.php' != $pagenow ) {
+        return;
+    }
+    
+    /*
+     * If the plugin settings have been updated flush rewrite rules
+     */
+    if ( isset( $_GET['page'] ) && ( 'kbte-testimonials' == $_GET['page'] ) ) {
+        $wp_rewrite->flush_rules();
+    }
+    
+}
+add_filter( 'admin_init', 'kbte_flush_rewrite_rules' );
