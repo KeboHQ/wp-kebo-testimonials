e091095923267ca8db9bdac4a21cf857fee15388
peterbooker <peter@kebopowered.com>
2013-12-04 17:22:17 +0000

    removed un-needed function

--- inc/misc.php
+++ inc/misc.php
@@ -2,15 +2,3 @@
 /* 
  * Kebo Testimonials - Misc/Helper Functions
  */
-
-/*
- * Flush Rewrite Rules
- * Used when changing the Testimonials page slug or installing/uninstalling the plugin.
- */
-function kbte_flush_rewrite_rules() {
-    
-    global $wp_rewrite;
-    
-    $wp_rewrite->flush_rules();
-    
-}
