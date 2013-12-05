3132166c3bd40cc7145c7dda89b7456d20293a4f
peterbooker <peter@kebopowered.com>
2013-12-05 03:42:17 +0000

    move settings menu under the testimonials post type menu

--- inc/menu.php
+++ inc/menu.php
@@ -14,9 +14,9 @@ if ( ! defined( 'KBTE_VERSION' ) ) {
 function kbte_testimonials_settings_page() {
 
     add_submenu_page(
-            'options-general.php', // Parent
-            __('Kebo Testimonials', 'kbte'), // Page Title
-            __('Kebo Testimonials', 'kbte'), // Menu Title
+            'edit.php?post_type=kbte_testimonials', // Parent
+            __('Settings', 'kbte'), // Page Title
+            __('Settings', 'kbte'), // Menu Title
             'manage_options', // Capability
             'kbte-testimonials', // Menu Slug
             'kbte_testimonials_settings_page_render' // Render Function
@@ -35,8 +35,8 @@ function kbte_testimonials_settings_page_render() {
     <div class="wrap">
         
         <?php screen_icon('options-general'); ?>
-        <h2><?php _e('Kebo Testimonials', 'kbte'); ?></h2>
-            <?php settings_errors( 'kbte-testimonials' ); ?>
+        <h2><?php _e('Testimonials - Settings', 'kbte'); ?></h2>
+        <?php settings_errors( 'kbte-testimonials' ); ?>
 
         <form method="post" action="options.php">
             <?php
