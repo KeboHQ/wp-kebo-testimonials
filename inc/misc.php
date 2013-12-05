7e43168772669f9e5ec294d559507b21aacc630d
peterbooker <peter@kebopowered.com>
2013-12-05 02:57:42 +0000

    fixed helper functions

--- inc/misc.php
+++ inc/misc.php
@@ -24,3 +24,48 @@ function kbte_flush_rewrite_rules() {
     
 }
 add_filter( 'admin_init', 'kbte_flush_rewrite_rules' );
+
+/*
+ * Helper Function - Returns Reviewer Name
+ */
+function kbte_get_reviewer_name() {
+    
+    global $post;
+    
+    $kbte_custom_meta = get_post_meta( $post->ID, 'kbte_testimonials_post_meta', true );
+    
+    $name = ( isset( $kbte_custom_meta['reviewer_name'] ) ) ? $kbte_custom_meta['reviewer_name'] : '' ;
+    
+    return esc_html( $name );
+    
+}
+
+/*
+ * Helper Function - Returns Reviewer URL
+ */
+function kbte_get_reviewer_url() {
+    
+    global $post;
+    
+    $kbte_custom_meta = get_post_meta( $post->ID, 'kbte_testimonials_post_meta', true );
+    
+    $url = ( isset( $kbte_custom_meta['reviewer_url'] ) ) ? $kbte_custom_meta['reviewer_url'] : '' ;
+    
+    return esc_url( $url );
+    
+}
+
+/*
+ * Helper Function - Returns Reviewer Rating
+ */
+function kbte_get_reviewer_rating() {
+    
+    global $post;
+    
+    $kbte_custom_meta = get_post_meta( $post->ID, 'kbte_testimonials_post_meta', true );
+    
+    $rating = ( isset( $kbte_custom_meta['reviewer_rating'] ) ) ? $kbte_custom_meta['reviewer_rating'] : null ;
+    
+    return absint( $rating );
+    
+}
