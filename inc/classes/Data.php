05b5ba95561f864fe97e24211caa7d18f47a3152
peterbooker <peter@kebopowered.com>
2013-12-06 23:17:06 +0000

    added form data related files

---
+++ inc/classes/Data.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Class to handle Form Data processing.
+ */
+
+if ( ! defined( 'KBTE_VERSION' ) ) {
+    header( 'HTTP/1.0 403 Forbidden' );
+    die;
+}
+
+/**
+ * Kebo_Data
+ */
+if ( ! class_exists( 'Kebo_Data' ) ) {
+    
+    class Kebo_Data {
+        
+        
+        
+    }
+    
+}
---
+++ inc/form-processing.php
@@ -0,0 +1,10 @@
+<?php
+/* 
+ * Kebo Testimonials - Process Form POST Data
+ */
+
+if ( ! defined( 'KBTE_VERSION' ) ) {
+    header( 'HTTP/1.0 403 Forbidden' );
+    die;
+}
+
